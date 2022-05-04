c7f56c4d66e02022a645e639680363442d3db0d8
Gianluigi Colameo <gianluigicolameo99@gmail.com>
2022-05-04 19:01:37 +0200

    Commit primi due esercizi

---
+++ Esercizio-1-fatto/style.cs
@@ -0,0 +1,25 @@
+/*Dichiarazione Variabili*/
+body {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+body div {
+  border: 1px solid red;
+}
+body div h1 {
+  color: green;
+  text-decoration: underline blue;
+  display: block;
+}
+body div section {
+  display: flex;
+  flex-direction: row;
+}
+body div section p {
+  border: 1px solid green;
+  padding: 2rem;
+}
+
+/*# sourceMappingURL=style.cs.map */
